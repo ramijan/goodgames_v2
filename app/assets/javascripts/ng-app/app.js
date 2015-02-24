8398ad05b0a55b3745b95808080a50b0604e104c
ramiburpee <rburpee@gmail.com>
2015-02-23 23:06:44 -0800

    Added stub pages for all planned pages (except modals) and hooked up ui-router

--- app/assets/javascripts/ng-app/app.js
+++ app/assets/javascripts/ng-app/app.js
@@ -6,11 +6,48 @@ angular
   }])
   .config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
     $stateProvider
-      .state('home', {
-        url: '/',
-        templateUrl: 'home.html',
-        controller: 'HomeCtrl'
-      });
+      .state('app', {
+        url: '',
+        templateUrl: 'app.html',
+        abstract: true
+      })
+        .state('app.home', {
+          url: '/',
+          templateUrl: 'home.html',
+          controller: 'HomeCtrl'
+        })
+        .state('app.intro', {
+          url: '/introduction',
+          templateUrl: 'intro.html'
+        })
+        .state('app.welcome', {
+          url: '/welcome',
+          templateUrl: 'welcome.html'
+        })
+        .state('app.search', {
+          url: '/search',
+          templateUrl: 'search.html'
+        })
+        .state('app.games', {
+          url: '/games',
+          templateUrl: 'games.html'
+        })
+        .state('app.game-detail', {
+          url: '/game/:id',
+          templateUrl: 'game-detail.html'
+        })
+        .state('app.profile', {
+          url: '/profile',
+          templateUrl: 'profile.html'
+        })
+        .state('app.profile-edit', {
+          url: '/profile-edit',
+          templateUrl: 'profile-edit.html'
+        })
+        .state('app.about', {
+          url: '/about',
+          templateUrl: 'about.html'
+        });
 
     $urlRouterProvider.otherwise('/');
 
