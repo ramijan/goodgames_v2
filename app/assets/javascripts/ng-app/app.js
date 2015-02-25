201885bb4ca7b5862115f40b30b18c1736c6c068
ramiburpee <rburpee@gmail.com>
2015-02-24 22:32:51 -0800

    Move navbar code to views folder to get access to current_user helper and add login/logout/signup links

--- app/assets/javascripts/ng-app/app.js
+++ app/assets/javascripts/ng-app/app.js
@@ -6,50 +6,44 @@ angular
   }])
   .config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
     $stateProvider
-      .state('app', {
-        url: '',
-        templateUrl: 'app.html',
-        controller: 'NavCtrl',
-        abstract: true
+      .state('home', {
+        url: '/',
+        templateUrl: 'home.html',
+        controller: 'HomeCtrl'
       })
-        .state('app.home', {
-          url: '/',
-          templateUrl: 'home.html',
-          controller: 'HomeCtrl'
-        })
-        .state('app.intro', {
-          url: '/introduction',
-          templateUrl: 'intro.html'
-        })
-        .state('app.welcome', {
-          url: '/welcome',
-          templateUrl: 'welcome.html'
-        })
-        .state('app.search', {
-          url: '/search',
-          templateUrl: 'search.html'
-        })
-        .state('app.games', {
-          url: '/games',
-          templateUrl: 'games.html'
-        })
-        .state('app.game-detail', {
-          url: '/game/:id',
-          templateUrl: 'game-detail.html',
-          controller: 'GameDetailCtrl'
-        })
-        .state('app.profile', {
-          url: '/profile',
-          templateUrl: 'profile.html'
-        })
-        .state('app.profile-edit', {
-          url: '/profile-edit',
-          templateUrl: 'profile-edit.html'
-        })
-        .state('app.about', {
-          url: '/about',
-          templateUrl: 'about.html'
-        });
+      .state('intro', {
+        url: '/introduction',
+        templateUrl: 'intro.html'
+      })
+      .state('welcome', {
+        url: '/welcome',
+        templateUrl: 'welcome.html'
+      })
+      .state('search', {
+        url: '/search',
+        templateUrl: 'search.html'
+      })
+      .state('games', {
+        url: '/games',
+        templateUrl: 'games.html'
+      })
+      .state('game-detail', {
+        url: '/game/:id',
+        templateUrl: 'game-detail.html',
+        controller: 'GameDetailCtrl'
+      })
+      .state('profile', {
+        url: '/profile',
+        templateUrl: 'profile.html'
+      })
+      .state('profile-edit', {
+        url: '/profile-edit',
+        templateUrl: 'profile-edit.html'
+      })
+      .state('about', {
+        url: '/about',
+        templateUrl: 'about.html'
+      });
 
     $urlRouterProvider.otherwise('/');
 
